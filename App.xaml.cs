e22d2ba97be69cabf3936ff040d46c7e3ae0d04b
Tomáš Motl <89769726+Heliaxx@users.noreply.github.com>
2024-10-10 22:30:52 +0200

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BCSH2_BDAS2_Armadni_Informacni_System
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
