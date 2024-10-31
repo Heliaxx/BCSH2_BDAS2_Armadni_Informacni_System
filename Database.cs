c64e54c5b78277b7208111f5eba7f0514744935c
Saunic <Saunic@DESKTOP-22OIF18>
2024-10-31 16:15:36 +0100

    Připojení k databázi

---
+++ Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace BCSH2_BDAS2_Armadni_Informacni_System
+{
+    internal class Database
+    {
+        private string connectionString;
+
+        public Database()
+        {
+            // Načtení connection stringu z app.config
+            connectionString = ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString;
+        }
+
+        public string TestConnection()
+        {
+            using (var connection = new OracleConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return "Připojeno k databázi.";
+                }
+                catch (Exception ex)
+                {
+                    return "Chyba připojení: " + ex.Message;
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BCSH2_BDAS2_Armadni_Informacni_System
 {
-    /// <summary>
-    /// Interakční logika pro MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            CheckDatabaseConnection();
+        }
+
+        private void CheckDatabaseConnection()
+        {
+            Database db = new Database();
+            string message = db.TestConnection();
+            StatusTextBlock.Text = message;
         }
     }
 }
