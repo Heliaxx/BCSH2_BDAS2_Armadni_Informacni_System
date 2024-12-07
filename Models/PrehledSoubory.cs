5bb2f6fd5c46ebdaecb901c121bff847f1ad92a3
Saunic <Saunic@DESKTOP-22OIF18>
2024-12-07 14:36:39 +0100

    BLOB kompletní

---
+++ Models/PrehledSoubory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BCSH2_BDAS2_Armadni_Informacni_System.Models
+{
+    public class PrehledSoubory
+    {
+        public int IdSoubor { get; set; }
+        public string NazevSouboru { get; set; }
+        public string TypSouboru { get; set; }
+        public string PriponaSouboru { get; set; }
+        public DateTime DatumNahrani { get; set; }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -96,5 +96,10 @@ namespace BCSH2_BDAS2_Armadni_Informacni_System
         {
             MainFrame.Content = new PrehledNadrizenychView();
         }
+
+        private void Soubory_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new PrehledSouboryView();
+        }
     }
 }
